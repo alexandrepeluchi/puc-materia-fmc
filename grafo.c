f7a757ea7d565870189b1dcbae9cbf68983b78ed
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-09-27 14:28:21 -0300

    Algumas alterações no código

--- grafo.c
+++ grafo.c
@@ -3,24 +3,25 @@
 #include <time.h>
 #define length 100
 
-struct t_thing {
-    int a[3][3];
+// Grafo C# possui 3 vertices e 2 arestas
+struct g_csharp {
+    int matriz[3][3];
 };
 
-struct t_thing retArr() {
-    struct t_thing thing = {
+struct g_csharp GrafoCSharp() {
+    struct g_csharp csharp = {
         {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+            {0, 1, 1},
+            {0, 0, 0},
+            {0, 0, 0}
         }
     };
 
-    return thing;
+    return csharp;
 }
 
 int main() {
-    struct t_thing thing = retArr();
+    struct g_csharp csharp = GrafoCSharp();
 
     int i, j;
 
@@ -28,7 +29,7 @@ int main() {
     {
         for(j = 0; j < 3; j++)
         {
-            printf("%d ",thing.a[i][j]);
+            printf("%d ", csharp.matriz[i][j]);
         }
         printf("\n");
     }
