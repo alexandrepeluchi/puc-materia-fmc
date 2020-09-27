c950d063bc520776a21ea3d608191aade8c6e294
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-09-27 16:49:10 -0300

    Adicionado menu básico para selecionar o grafo

--- grafo.c
+++ grafo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define length 100
 
 // Grafo C# possui 3 vertices e 2 arestas
 struct g_csharp {
@@ -20,10 +19,49 @@ struct g_csharp GrafoCSharp() {
     return csharp;
 }
 
-int main() {
+int main()
+{
+    // Adicionar essa linha dentro de um switch talvez
     struct g_csharp csharp = GrafoCSharp();
-
     int i, j;
+
+    // Controle da aplicacao
+    int quit = 0;
+    int grafoSelecionado = 0;
+
+    while(quit != 1)
+    {
+        // Exibicao do Menu
+        printf("\n--- Trabalho de FMC - Grafos --- \n\n\n");
+        printf("Selecione um dos grafos a seguir:\n\n");
+        printf("[1] C#\n");
+
+
+        printf("[0] Encerrar aplicacao")
+
+        printf("\n\n");
+        printf("Digite a sua opcao: ");
+        scanf("%d", &grafoSelecionado);
+
+        switch(grafoSelecionado)
+        {
+            case 0:
+                printf("\nAplicacao sendo encerrada...\n\n");
+                quit = 1;
+            break;
+
+            case 1:
+
+            break;
+
+            default:
+                printf("\nGrafo selecionado nao existe, por favor digite uma opcao valida!\n\n");
+            break;
+        }
+        system("pause");
+        system("cls");
+    }
+
 
     for(i = 0; i < 3; i++)
     {
